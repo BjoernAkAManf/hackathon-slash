cc803dfc64df3eaa18e0512a5b380950bcfac284
evelina <kazandgiy@gmail.com>
2020-09-26 17:53:08 +0200

    Add models

--- web/charityswipe/models.py
+++ web/charityswipe/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Interest(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class Foundation(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    interests = models.ForeignKey(Interest, on_delete=models.CASCADE, related_name='foundation')
+
+class User(User):
+    name = models.CharField(max_length=100)
+    interests = models.ManyToManyField(Interest)
 
 # Create your models here.
